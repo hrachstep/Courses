ddd4a02fb0fda669009b79a12414017d254a3c37
hrachstep <hrastepanyan@gmail.com>
2015-04-15 15:57:34 +0400

    Java: 2

--- Java/Project1/src/Package1/TestClass.java
+++ Java/Project1/src/Package1/TestClass.java
@@ -3,7 +3,17 @@ package Package1;
 public class TestClass {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		//int number1 = 25;
+		//double number2 = 35.5;
+		//char character1 = 'a';
+		//boolean bool1 = true;
+		
+		String text = "Hello";
+		
+		System.out.println(text);
+		
+		
+		
 	}
 	
 }
