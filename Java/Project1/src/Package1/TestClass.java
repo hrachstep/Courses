9ca6b8fc2b2dea7ee62f5fcefe9b0752813c6234
hrachstep <hrastepanyan@gmail.com>
2015-04-15 15:57:43 +0400

    Revert "Java: 2"
    This reverts commit ddd4a02fb0fda669009b79a12414017d254a3c37.

--- Java/Project1/src/Package1/TestClass.java
+++ Java/Project1/src/Package1/TestClass.java
@@ -3,17 +3,7 @@ package Package1;
 public class TestClass {
 	
 	public static void main(String[] args) {
-		//int number1 = 25;
-		//double number2 = 35.5;
-		//char character1 = 'a';
-		//boolean bool1 = true;
-		
-		String text = "Hello";
-		
-		System.out.println(text);
-		
-		
-		
+		System.out.println("Hello World!");
 	}
 	
 }
