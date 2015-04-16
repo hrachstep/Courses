9853a136e56d995ab6320e0b96bf0c8b0575119a
hrachstep <hrastepanyan@gmail.com>
2015-04-16 17:03:31 +0400

    checkin input.java

---
+++ Java/Input.java
@@ -0,0 +1,19 @@
+ppackage TestPackage;
+
+import java.util.Scanner;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Please Enter the number -> ");
+		String number = input.nextLine();
+		
+		System.out.println(number);
+		
+			
+	}
+
+}
+}
