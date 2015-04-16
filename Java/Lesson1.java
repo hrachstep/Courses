55bfc4bc8f9ccd9d5d4b2ca68741986191168e6d
hrachstep <hrastepanyan@gmail.com>
2015-04-16 12:35:31 +0400

    First commit to java

---
+++ Java/Lesson1.java
@@ -0,0 +1,18 @@
+package TestPackage;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		
+		int index = 0;
+		
+		while(index < 10) {
+			System.out.print("Index is:");
+			System.out.println(index);
+			++index;
+		}
+		
+			
+	}
+
+}
